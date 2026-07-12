ebf9a3de2b49436cbdfbbd592dd9259e7e804250
LAXIT JANGID <139094932+Laxit85@users.noreply.github.com>
2026-07-12 11:10:32 +0530

    Delete backend/src directory

--- backend/src/main/java/com/assetflow/booking/BookingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.assetflow.booking;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-    List<Booking> findByResourceAssetIdAndStatusNot(Long resourceAssetId, String status);
-}
--- backend/src/main/java/com/assetflow/masters/AssetCategoryRepository.java
+++
@@ -1,5 +0,0 @@
-package com.assetflow.masters;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AssetCategoryRepository extends JpaRepository<AssetCategory, Long> {}
